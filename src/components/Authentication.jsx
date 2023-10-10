bb14670d3de90c0d8d079bb43027f88f930579c1
Ladalo-Daniel <balaladalo@gmail.com>
2023-10-10 10:15:25 +0100

    hot fix

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,9 +1,11 @@
 "use client";
+
 import { Button } from "@nextui-org/react";
+
 export default function Authentication() {
   return (
     <main className="flex min-h-screen  flex-col items-center  p-3">
-      <Button className=" bg-darkOrange font-poppins my-3 font-medium">Welcome to watin Dey Sup</Button>
+      <Button className=" bg-darkOrange font-poppins my-3 font-medium ">Welcome to WetinDeySup!</Button>
     </main>
   );
 }
--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -16,9 +16,9 @@ export default function BottomNavbar() {
   const router = useRouter()
   return (
     <>
-      <nav className="flex items-center bg-white dark:bg-darkSlate justify-around z-[99] lg:hidden gap-x-4 w-full py-3 fixed bottom-0 shadow-lg">
+      <nav className="flex items-center bg-white dark:bg-darkSlate justify-around z-[99] lg:hidden  gap-x-4 w-full py-3 fixed bottom-0 shadow-lg">
         {Navigation.map((items, index) => (
-          <div key={index} onClick={() => router.push(items.Link)} className="flex items-center flex-col gap-y-2">
+          <div key={index} onClick={() => router.push(items.Link)} className="flex items-center flex-col lg:flex-row lg:gap-x-2 lg:items-center gap-y-2">
             <span className={` text-black dark:text-white text-xl`}>{items.icon}</span>
             <p className={` text-black   dark:text-white font-poppins text-sm`}>{items.name}</p>
           </div>
