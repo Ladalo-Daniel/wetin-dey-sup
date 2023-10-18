e300dd6bd1856ee72a52aef6e47959f53c5ad3cb
Ladalo-Daniel <balaladalo@gmail.com>
2023-10-18 23:31:58 +0100

    resolved a merge conflict

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -2,20 +2,9 @@ import Mainlayout from "@/components/Mainlayout";
 import TimeLineWrapper from "@/components/TimelineUI/TimeLineWrapper";
 
 export default function TimelinePage() {
-  return (
-<<<<<<< HEAD
+return (
      <Mainlayout>
         <TimeLineWrapper />
      </Mainlayout>
   )
-=======
-    <Mainlayout>
-      <section className=" min-h-screen">
-        <SearchEvents />
-        <TagsButton />
-        <TodayWrapper />
-      </section>
-    </Mainlayout>
-  );
->>>>>>> feba10f34ae66be0307b45c709b364e196eca9fa
-}
+  }
