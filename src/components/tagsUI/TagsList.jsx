bb19a213df1c42b083fcc3fed13131dae2045488
Ladalo-Daniel <balaladalo@gmail.com>
2023-10-20 11:10:08 +0100

    implemented range custom hook

--- src/components/tagsUI/TagsList.jsx
+++ src/components/tagsUI/TagsList.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import TagItem from './TagItem'
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import range from '@/utils/range';
+import toast from 'react-hot-toast';
 
 function TagsList() {
+  const rand = range(0, 10)
   return (
     <div className='py-4 my-2 px-4 flex flex-col gap-4'>
-      <TagItem />
-      <TagItem />
-      <TagItem />
-      <TagItem />
-      <TagItem />
-      <TagItem />
+      {
+        rand?.map(item => (
+          <TagItem key={item} />
+        ))
+      }
     </div>
   )
 }
