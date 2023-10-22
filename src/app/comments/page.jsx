96656e2acd4d2fde054b2853c6fea71d4872b0df
Chinaza <elechukwujosh3@gmail.com>
2023-10-22 02:42:09 +0200

    On branch chinaza  Your branch is up to date with 'origin/chinaza'.
     Changes to be committed:
    	modified:   src/app/comments/page.jsx
    	new file:   src/app/tags/events/[id]/page.jsx
    	modified:   src/app/tags/page.jsx
    	modified:   src/components/Tags/Tags.jsx
    	modified:   src/components/TopNavbar.jsx
    
    Few updates from Chinaza

--- src/app/comments/page.jsx
+++ src/app/comments/page.jsx
@@ -1,15 +1,27 @@
-<<<<<<< HEAD
-import CommentsWrapper from "@/components/CommentsUI/CommentsWrapper";
-=======
 import CommentSection from "@/components/CommentsUI/CommentSection";
 import EventCard from "@/components/CommentsUI/EventCard";
->>>>>>> feba10f34ae66be0307b45c709b364e196eca9fa
 import Mainlayout from "@/components/Mainlayout";
 
 export default function CommentsPage() {
   return (
      <Mainlayout>
-        <CommentsWrapper />
+        <CommentSection />
      </Mainlayout>
   )
 }
+
+// <<<<<<< HEAD
+// import CommentsWrapper from "@/components/CommentsUI/CommentsWrapper";
+// =======
+// import CommentSection from "@/components/CommentsUI/CommentSection";
+// import EventCard from "@/components/CommentsUI/EventCard";
+// >>>>>>> feba10f34ae66be0307b45c709b364e196eca9fa
+// import Mainlayout from "@/components/Mainlayout";
+
+// export default function CommentsPage() {
+//   return (
+//      <Mainlayout>
+//         <CommentsWrapper />
+//      </Mainlayout>
+//   )
+// }
