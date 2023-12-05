ae234604198222cd18849e2785f5768d1640d9ae
nehemiahcode <codehubby@gmail.com>
2023-12-05 13:03:01 +0100

    still working on uploadthing

--- src/app/api/uploadthing/route.js
+++ src/app/api/uploadthing/route.js
@@ -2,7 +2,11 @@ import { createNextRouteHandler } from "uploadthing/next";
  
 import { ourFileRouter } from "./core";
  
-// Export routes for Next App Router
+// Export routes for Next App Router 
 export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
+  config: {
+    uploadthingId: "someId",
+    uploadthingSecret: "someSecret",
+  },
 });
